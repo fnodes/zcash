8dd563d217ffc965d71b9ff0e13a14786d2e5877
Tyler Baker <tyler@foundries.io>
2019-01-02 21:08:09 -0800

    oops
    Signed-off-by: Tyler Baker <tyler@foundries.io>

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ docker pull fullnodes/zcash:latest
 docker stop zcashd
 docker rm zcashd
 docker volume create zcash
-docker run -dit --name bitcoind --restart=always -e 'ZEC_RPCUSER=admin' -e 'ZEC_RPCPASSWORD=changeme' -e 'ZEC_TXINDEX=1' -e 'ZEC_DISABLE_WALLET=0' -v zcash:/root/.zcash fullnodes/zcash:latest
+docker run -dit --name zcashd --restart=always -e 'ZEC_RPCUSER=admin' -e 'ZEC_RPCPASSWORD=changeme' -e 'ZEC_TXINDEX=1' -e 'ZEC_DISABLE_WALLET=0' -v zcash:/root/.zcash fullnodes/zcash:latest
